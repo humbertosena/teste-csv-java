10a3c71a31e223653c18afa59dfdb0e7ee87fa93
humbertosena <humbertosena@gmail.com>
2017-01-06 13:21:49 -0200

    Primeiras linhas

--- src/main/java/ReadData.java
+++ src/main/java/ReadData.java
@@ -1,8 +1,32 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ReadData {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		try {
+			Path currentRelativePath = Paths.get(".");
+			String s = currentRelativePath.toAbsolutePath().toString();
+			
+			File file = new File(s + "//resources//cidades.csv");
+			FileReader fr = new FileReader(file);
+		    BufferedReader br = new BufferedReader(fr);
+		    String line;
+		    while((line = br.readLine()) != null){
+		            System.out.println(line);
+		    }
+		    br.close();
+		    fr.close();			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 
 	}
 
